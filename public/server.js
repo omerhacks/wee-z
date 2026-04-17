3be2e7508d779c72dc88c200c308d4ba41c7a256
omerhacks <117520288+omerhacks@users.noreply.github.com>
2026-04-17 16:35:56 +0500

    Update server.js

--- public/server.js
+++ public/server.js
@@ -48,12 +48,11 @@ app.post('/order', (req, res) => {
 
   res.json({ success: true });
 });
+app.use(express.static('public'));
 
 // ✅ IMPORTANT: always serve frontend
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
+});, () => {
   console.log("Server running on port " + PORT);
 });
